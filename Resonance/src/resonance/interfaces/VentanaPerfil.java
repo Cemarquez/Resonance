8bd7a726fdfd1c5d06c82a4a1e90e62ef1322d2d
admin <admin@BILISB06.uniquindio.edu.co>
2019-10-28 09:57:40 -0500

    Se crea borrador de VentanaPerfil

---
+++ Resonance/src/resonance/interfaces/VentanaPerfil.java
@@ -0,0 +1,64 @@
+package resonance.interfaces;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+
+public class VentanaPerfil extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaPerfil frame = new VentanaPerfil();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public VentanaPerfil() {
+		getContentPane().setBackground(Color.DARK_GRAY);
+
+		this.setExtendedState(MAXIMIZED_BOTH);
+		Dimension tamano = Toolkit.getDefaultToolkit().getScreenSize();
+		setSize(tamano);
+		getContentPane().setSize(tamano);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 1599, 59);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel imgInicio = new JLabel("New label");
+		imgInicio.setIcon(new ImageIcon(VentanaPerfil.class.getResource("/imagenes/Logo1Reducido.png")));
+		imgInicio.setBounds(10, 11, 53, 48);
+		panel.add(imgInicio);
+		
+		JLabel lblInicio = new JLabel("Inicio");
+		lblInicio.setFont(new Font("Segoe UI", Font.BOLD, 20));
+		lblInicio.setBounds(73, 15, 52, 31);
+		panel.add(lblInicio);
+	}
+
+}
